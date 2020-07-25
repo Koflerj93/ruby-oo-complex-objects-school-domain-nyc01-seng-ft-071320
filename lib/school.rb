d0ff4651d526452ddf4cc0fd3ca1c7d4fdf26ff6
Joseph Kofler <koflerj93@gmail.com>
2020-07-25 17:05:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+  
+  roster = {}
+  
+  def add_student(student_name, grade)
+    roster << add_student  
+  end 
+    
+    def grade
+        @grade = grade
+        
+    end 
+    
+end 
